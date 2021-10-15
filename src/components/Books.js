a8502f5cc71791cd4a2b35694f38d911c36c7406
emmanuellekamwa <61285247+emmanuellekamwa@users.noreply.github.com>
2021-10-15 09:17:14 +0100

    Fix import statement

--- src/components/Books.js
+++ src/components/Books.js
@@ -2,7 +2,7 @@
 
 import { React, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getBooks } from '../redux/books/Books';
+import { getBooks } from '../redux/books/books';
 import BookList from './bookList';
 import BookAdd from './BookAdd';
 
